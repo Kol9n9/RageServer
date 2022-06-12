dfd34289159d5380e207c1588b50c173518690a3
NikolayS <slezenkonikolay@mail.ru>
2022-06-12 20:15:51 +0700

    Исправил ошибки и структуризировал тестовый функционал

---
+++ RageServer/ChatSystem/TestChatSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace RageServer.ChatSystem
+{
+    class TestChatSystem : Script
+    {
+        private readonly List<TestJobChat> jobChats;
+        public TestChatSystem()
+        {
+            jobChats = new List<TestJobChat>();
+            jobChats.Add(new TestJobChat("Созданный"));
+        }
+        [Command("j", GreedyArg = true)]
+        private void SendJobChatMessage(Player player, int Id, string message)
+        {
+            TestJobChat chat = jobChats.Find(x => x.Id == Id);
+            if (chat == null)
+            {
+                player.SendChatMessage("Данный чат не найден");
+                return;
+            }
+            chat.SendChatMessage(player, message);
+        }
+        [Command("jc")]
+        private void CreateJobChat(Player player, string name)
+        {
+            TestJobChat chat = new TestJobChat(name);
+            chat.AddPlayerToChat(player);
+            jobChats.Add(chat);
+            player.SendChatMessage($"Вы создали чат с Id = ~g~{chat.Id} ");
+        }
+        [Command("ji")]
+        private void ConnectToJobChat(Player player, int Id)
+        {
+            TestJobChat chat = jobChats.Find(x => x.Id == Id);
+            if (chat == null)
+            {
+                player.SendChatMessage("Данный чат не найден");
+                return;
+            }
+            chat.AddPlayerToChat(player);
+            player.SendChatMessage($"Вы присоединились к чату с Id = ~g~{chat.Id} ");
+        }
+    }
+}
--- RageServer/Test.cs
+++ RageServer/Test.cs
@@ -11,46 +11,15 @@ namespace RageServer
     /// </summary>
     class Test : Script
     {
-        private readonly List<TestJobChat> jobChats;
+        
         public Test()
         {
-            jobChats = new List<TestJobChat>();
+            
         }
         [ServerEvent(Event.PlayerConnected)]
         private void OnPlayerConnected(Player player)
         {
             player.SendChatMessage("OnPlayerConnected");
-        }
-        
-        [Command("j",GreedyArg = true)]
-        private void SendJobChatMessage(Player player, int Id, string message)
-        {
-            TestJobChat chat = jobChats.Find(x => x.Id == Id);
-            if(chat == null)
-            {
-                player.SendChatMessage("Данный чат не найден");
-                return;
-            }
-            chat.SendChatMessage(player, message);
-        }
-        [Command("jc")]
-        private void CreateJobChat(Player player, string name)
-        {
-            TestJobChat chat = new TestJobChat(name);
-            chat.AddPlayerToChat(player);
-            player.SendChatMessage($"Вы создали чат с Id = ~g~{chat.Id} ");
-        }
-        [Command("ji")]
-        private void ConnectToJobChat(Player player, int Id)
-        {
-            TestJobChat chat = jobChats.Find(x => x.Id == Id);
-            if (chat == null)
-            {
-                player.SendChatMessage("Данный чат не найден");
-                return;
-            }
-            chat.AddPlayerToChat(player);
-            player.SendChatMessage($"Вы присоединились к чату с Id = ~g~{chat.Id} ");
         }
     }
 }

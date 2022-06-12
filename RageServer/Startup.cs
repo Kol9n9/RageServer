fd7d9834f8e7fd04fe5f42e57633efc3cbc5c64f
NikolayS <slezenkonikolay@mail.ru>
2022-06-12 18:26:08 +0700

    Первоначальнвя настройка сервера

---
+++ RageServer/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+namespace RageServer
+{
+    /// <summary>
+    /// Точка входа
+    /// </summary>
+    class Startup : Script
+    {
+        private readonly int major = 0;
+        private readonly int minor = 0;
+        private readonly int realese = 0;
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("Запуск сервера...");
+        }
+    }
+}
---
+++ RageServer/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace RageServer
+{
+    /// <summary>
+    /// Класс для проверки разрабатываемого функционала
+    /// </summary>
+    class Test : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("OnPlayerConnected");
+        }
+    }
+}
